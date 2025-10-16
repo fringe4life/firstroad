f2f5a0a49f7da4519a5a5dce54a22cb10978a72f
Coinnich <>
2025-10-17 00:21:52 +1300

    chore: migrated lint-staged config to typescript file

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('lint-staged').Configuration} */
+import type { Configuration } from "lint-staged";
 export default {
   "*.{js,jsx,ts,tsx,json,jsonc,css,scss,md,mdx}":
     "bun x ultracite fix --unsafe true",
   "**/*.{ts,tsx}": () => "bun run type",
-};
+} satisfies Configuration;
